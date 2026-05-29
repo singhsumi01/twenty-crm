f494880620998a8e0f261b0f9841aa333c983c48
rashad <rashad.karanouh@gmail.com>
2026-05-29 07:24:55 +0400

    feat(website): thread locale to PartnerCard for detail-page links

--- packages/twenty-website/src/app/[locale]/partners/list/page.tsx
+++ packages/twenty-website/src/app/[locale]/partners/list/page.tsx
@@ -23,8 +23,9 @@ type PartnersMarketplacePageProps = {
 export default async function PartnersMarketplacePage({
   params,
 }: PartnersMarketplacePageProps) {
-  const [, stats, livePartners] = await Promise.all([
+  const [, { locale }, stats, livePartners] = await Promise.all([
     getRouteI18n(params),
+    params,
     fetchCommunityStats(),
     getPartners(),
   ]);
@@ -47,7 +48,7 @@ export default async function PartnersMarketplacePage({
       <MarketplaceHeader />
 
       <Suspense fallback={null}>
-        <MarketplaceClient partners={livePartners} />
+        <MarketplaceClient partners={livePartners} locale={locale} />
       </Suspense>
     </>
   );
